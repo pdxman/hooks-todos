87e8c5251576e87280181d187162d1981b32be5a
Crawford Vining <crawfordvining@fm-cravin2-mac.home>
2021-02-19 12:47:03 -0800

    added more styling

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -13,7 +13,7 @@ export default function TodoList(){
             <h1 className="text-bold">{title}</h1>
             <ul className="flex-list list-reset text-white p-0">
                 {state.todos.map(todo => (
-                      <li key={todo.id} className="border-custom flex items-center bg-green-500 border-black border-2 my-2 py-4">
+                      <li key={todo.id} className="border-custom flex items-center border-black border-2 my-2 py-4">
                            <span 
                             onDoubleClick={() => dispatch({ type: "TOGGLE_TODO", payload: todo })}
                             className={`flex-1 ml-12 cursor-pointer ${todo.complete && "line-through text-black"}`}>{todo.text}</span> 
